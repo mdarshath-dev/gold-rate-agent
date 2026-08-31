6525e05b03b00380cd553b348dae33d28bf2944f
mdarshath-dev <mdarshath18@gmail.com>
2026-08-31 23:04:44 +0530

    added architecture using https://github.com/tt-a1i/archify.git and codex. also removed test files

--- env-test.js
+++
@@ -1,9 +0,0 @@
-require("dotenv").config();
-
-const token = process.env.TELEGRAM_BOT_TOKEN;
-
-if (token) {
-    console.log("Telegram token was loaded successfully!");
-} else {
-    console.log("Telegram token was NOT found.");
-}
--- types.js
+++
@@ -1,4 +0,0 @@
-const goldRate = Number("14800.00");
-
-console.log(goldRate);
-console.log(typeof goldRate);
